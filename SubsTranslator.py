d6761b9b5227b4605090fb53c71d486c6484f186
_dan_ <117082184+daniiren@users.noreply.github.com>
2022-11-05 10:25:50 +0200

    Update SubsTranslator.py

--- SubsTranslator.py
+++ SubsTranslator.py
@@ -1,34 +1,55 @@
-from python_translator import Translator
+import os
+import deepl
+import srt
 
-print('\n')
 print("****************************")
 print("* Welcome to SubsTransator *")
 print("****************************")
+print("\nCase sensitive! (accepted example : EL or el)")
+print('''Supported Languages : 
+    BG - Bulgarian
+    CS - Czech
+    DA - Danish
+    DE - German
+    EL - Greek
+    EN - English
+    ES - Spanish
+    ET - Estonian
+    FI - Finnish
+    FR - French
+    HU - Hungarian
+    ID - Indonesian
+    IT - Italian
+    JA - Japanese
+    LT - Lithuanian
+    LV - Latvian
+    NL - Dutch
+    PL - Polish
+    PT - Portuguese (all Portuguese varieties mixed)
+    RO - Romanian
+    RU - Russian
+    SK - Slovak
+    SL - Slovenian
+    SV - Swedish
+    TR - Turkish
+    UK - Ukrainian
+    ZH - Chinese
+''')
 
-inputSrtPath = input("\nEnter input srt path : ")
-inputLanguage = input("\nFrom which language : ")
-outputLanguage = input("To wich language : ")
-
-fin = open(inputSrtPath[1:-2], 'r+')
-fout = open(outputLanguage + ".srt", 'w')
-fin.write('\n')
-
-counter = 0
+inputSrtPath = input("Enter input srt path : ")[1:-2]
+outputLanguage = input("\nTo wich language : ").upper()
+srtName = os.path.basename(inputSrtPath)
+fin = open(inputSrtPath, 'r+')
+fout = open(srtName[:-4] + "_greek" + srtName[-4:], 'w')
+translator = deepl.Translator("your deepl api key")
 finalSrt = ""
-plainText = ""
-translator = Translator()
 
-for line in fin.readlines() :
-	counter += 1
-	if (line == '\n') :
-		plainText = str(translator.translate(plainText, outputLanguage, inputLanguage))
-		finalSrt += plainText + "\n\n"
-		plainText = ""
-		counter = 0
-	elif (counter > 2) :
-		plainText += line
-	else :
-		finalSrt += line
+print("Perfect! Please be patient while we are doing our magic!...")
+for currentSrt in srt.parse(fin) :
+	finalSrt += str(currentSrt.index) + '\n'
+	finalSrt += str(currentSrt.start).replace('.', ',')[:-3] + " --> " + str(currentSrt.end).replace('.', ',')[:-3] +  '\n'
+	finalSrt += translator.translate_text(currentSrt.content.replace('\n', ' '), target_lang = outputLanguage).text + '\n\n'
+print("All ready! Enjoy the movie :)")
 
 fout.write(finalSrt)
 fin.close()
